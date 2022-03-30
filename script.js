5a89a279e84c6a6938e6606e65d7092fb93acb8e
Erich Elste <erichelste@yahoo.com>
2022-03-30 13:35:38 -0400

    added welcome alert and choice prompt

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var choices = [ "Rock", "Paper", "Scissors"]
+
+function welcome(){
+    console.log("start")
+    alert("Welcome to Rock, Paper, Scissors!")
+}
+
+function choose(){
+    console.log("start")
+    prompt("Please pick Rock, Paper or Scissors")
+}
+
+welcome()
+choose()
+
